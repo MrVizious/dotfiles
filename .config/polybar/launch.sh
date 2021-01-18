69cdc28e926f1174d19d2f3e599a7a7b64999180
Javier <mrvizious@gmail.com>
2021-01-18 14:29:41 +0000

    Simple polybar working

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,13 +3,16 @@
 # Terminate already running bar instances
 killall -q polybar
 
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+ 
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch "mrvizious" bar on all monitors
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mrvizious &
+  for m in $(polybar --list-monitors | cut -d":" -f1); do
+      MONITOR=$m polybar --reload mrvizious &
   done
 else
   polybar --reload mrvizious &
